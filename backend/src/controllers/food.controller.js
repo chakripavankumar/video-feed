c610a748eb01ccc9baf9730e1484078cd69e114f
Pavan Kumar <siriyalapavankumar@gmail.com>
2025-09-11 02:25:09 +0530

    tested video upload  useing multer

--- backend/src/controllers/food.controller.js
+++ backend/src/controllers/food.controller.js
@@ -1,6 +1,14 @@
 const foodModel = require("../model/food.modal");
 
-async function createFood(req, res) {}
+async function createFood(req, res) {
+  console.log("test");
+  console.log(req.foodPartner);
+  console.log(req.body);
+
+  console.log(req.file);
+
+  res.status(201).json({ message: "Food created successfully" });
+}
 
 async function getFoodItems(req, res) {}
 
